d96c991f6a889e6afd7436d7a5fa2fac43f22ae1
Ignacio Pascual <ipascual@unexpectedit.com>
2015-04-27 17:45:38 -0500

    jenkins production fix

--- server/config/express.js
+++ server/config/express.js
@@ -26,9 +26,8 @@ module.exports = function(app) {
   app.use(bodyParser.json());
   app.use(methodOverride());
   app.use(cookieParser());
-  
+
   if ('production' === env) {
-    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     app.use(express.static(path.join(config.root, 'public')));
     app.set('appPath', config.root + '/public');
     app.use(morgan('dev'));
@@ -39,7 +38,10 @@ module.exports = function(app) {
     app.use(express.static(path.join(config.root, '.tmp')));
     app.use(express.static(path.join(config.root, 'client')));
     app.set('appPath', 'client');
-    app.use(morgan('dev'));
     app.use(errorHandler()); // Error handler - has to be last
+  }
+
+  if('development' === env) {
+    //app.use(morgan('dev'));
   }
 };
